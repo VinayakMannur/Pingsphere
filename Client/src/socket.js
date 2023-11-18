e65dd95e6e509a067780fdbf20b53c1180ef70f6
VinayakMannur <Vinayakmannur20@gmail.com>
2023-11-18 11:18:34 +0530

    added socket in frontend

--- Client/src/socket.js
+++ Client/src/socket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client'
 let socket
 
 const connectSocket = (user_id)=>{
-    socket = io("http://localhost:5000",{
+    socket = io("https://pingsphere.netlify.app",{
         query: `user_id=${user_id}`
     })
 }
