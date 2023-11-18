d0bf4e48b1c29142c32af47c54a157b8cfce1ff6
VinayakMannur <Vinayakmannur20@gmail.com>
2023-11-18 11:47:48 +0530

    changed socket and config

--- Client/src/socket.js
+++ Client/src/socket.js
@@ -3,9 +3,11 @@ import io from 'socket.io-client'
 let socket
 
 const connectSocket = (user_id)=>{
-    socket = io("https://pingsphere.netlify.app",{
+    socket = io("https://pingsphere-server-gkfs.onrender.com",{
         query: `user_id=${user_id}`
     })
 }
 
 export {socket, connectSocket}
+
+// "https://pingsphere-server-gkfs.onrender.com"
