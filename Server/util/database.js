ed0a1964a1f29b2f5acea9c5028bcffa2b2fb2d3
VinayakMannur <Vinayakmannur20@gmail.com>
2023-12-07 12:09:42 +0530

    added dialect options

--- Server/util/database.js
+++ Server/util/database.js
@@ -6,7 +6,10 @@ const sequelize = new Sequelize(
   process.env.DATABASE_PASSWORD,
   {
     dialect: "mysql",
-    host: 3306,
+    host: process.env.DATABASE_HOST,
+    dialectOptions: {
+      connectTimeout: 90000, // Set a higher value in milliseconds
+    },
   }
 );
 
